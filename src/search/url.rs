3ca4a14e62b24154a2a9a9748f3fa83602cf5891
TwentyXX <twenty.takumi1024@gmail.com>
2025-02-15 23:04:18 +0900

    feature: implement search-url-builder

---
+++ src/search/url.rs
@@ -0,0 +1,17 @@
+
+pub(crate) fn build_search_url(query: &str) -> String {
+	format!(
+		"https://w.atwiki.jp/hmiku/search?andor=and&keyword={}",
+		urlencoding::encode(query)
+	)
+}
+
+#[test]
+fn test_build_search_url() {
+	let query = "孤独毒毒";
+	let url = build_search_url(query);
+	assert_eq!(
+		url,
+		"https://w.atwiki.jp/hmiku/search?andor=and&keyword=%E5%AD%A4%E7%8B%AC%E6%AF%92%E6%AF%92"
+	);
+}
